4124c53a32388d45757b09eec60f8eec01e3cb8a
AayushJain27 <ayush@serviceplug.in>
2026-05-14 22:23:47 +0530

    new chnages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import "./App.css";
 import Dashboard from "./component/Dashboard";
 import { Route, Routes } from "react-router-dom";
-import PrivacyPolicy from "./component/PrivacyPolicy";
-import RefundPolicy from "./component/RefundPolicy";
-import InvestorCharter from "./component/InvestorCharter";
-import StandardDisclosures from "./component/Disclosure";
-import GrievanceRedressal from "./component/Grievance";
-import InternalPolicies from "./component/InternalPolices";
-import CodeOfConduct from "./component/CodeOfConduct";
-import TermsAndConditions from "./component/TermsAndCondition";
-import ContactUs from "./component/ContactUs";
-import ComplaintData from "./component/ComplaintData";
-import ResearchServices from "./component/ResearchServices";
 
 function App() {
   return (
   <Routes>
   <Route path="/" element={<Dashboard />} />
-  <Route path="/disclosure" element={<StandardDisclosures />} />
+  {/* <Route path="/disclosure" element={<StandardDisclosures />} />
   <Route path="/grievance" element={<GrievanceRedressal />} />
   <Route path="/internalPolicies" element={<InternalPolicies />} />
   <Route path="/codeOfConduct" element={<CodeOfConduct />} />
@@ -27,7 +16,7 @@ function App() {
   <Route path="/privacyPolicy" element={<PrivacyPolicy />} />
   <Route path="/termsAndConditions" element={<TermsAndConditions />} />
   <Route path="/refundPolicy" element={<RefundPolicy />} />
-  <Route path="/contact" element={<ContactUs />} />
+  <Route path="/contact" element={<ContactUs />} /> */}
 </Routes>
   );
 }
